964ab24fb001e7c57a7b495b1fd0c8e9c554d369
Tanmoy0077 <tanmoy.pradhan0077@gmail.com>
2024-07-14 22:14:21 +0530

    Added logo and navigation in login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sample_app/components/my_textfield.dart';
 import 'package:sample_app/components/square_tile.dart';
+import 'package:sample_app/pages/first_page.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
@@ -11,20 +13,29 @@ class LoginPage extends StatelessWidget {
   final passwordController = TextEditingController();
 
   void signInUser() {}
+  void registerUser() {}
+  void forgotPassword() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.grey[300],
+      backgroundColor: Color.fromRGBO(232, 236, 239, 1),
       body: ListView(
         children: [
-          Container(
-            height: 250,
-            decoration: BoxDecoration(color: Colors.purple[300]),
+          Image.asset(
+            'lib/assets/images/bicycle.png',
+            height: 280,
+          ),
+          Center(
+            child: Text("Welcome",
+                style: GoogleFonts.baskervville(
+                    fontSize: 30,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold)),
           ),
           SizedBox(
-            height: 20,
+            height: 15,
           ),
           MyTextfield(
             controller: usernameController,
@@ -47,18 +58,24 @@ class LoginPage extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Text(
-                  "Forgot Password?",
-                  style: TextStyle(fontSize: 15),
+              GestureDetector(
+                onTap: forgotPassword,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                    "Forgot Password?",
+                    style: TextStyle(fontSize: 15),
+                  ),
                 ),
               ),
             ],
           ),
           SizedBox(height: 20),
           GestureDetector(
-              onTap: signInUser,
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => FirstPage()));
+              },
               child: Container(
                 padding: EdgeInsets.all(20),
                 margin: EdgeInsets.symmetric(horizontal: 20),
@@ -74,49 +91,71 @@ class LoginPage extends StatelessWidget {
                         fontSize: 18),
                   ),
                 ),
-              )
-            ),
-
-            SizedBox(height: 40,),
-
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Divider(
-                      thickness: 0.8,
-                      color: Colors.grey[500],
-                    ),
-                  ),
-                  Text(
-                    "Or continue with",
-                    style: TextStyle(
-                      fontSize: 15,
-                    ),
-                  ),
-                  Expanded(
-                    child: Divider(
-                      thickness: 0.8,
-                      color: Colors.grey[500],
-                    ),
-                  )
-                ],
-              ),
-            ),
-
-            SizedBox(height: 10,),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+              )),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
               children: [
-                SquareTile(imagePath: "lib/assets/images/google.png"),
-                SizedBox(width: 25,),
-                SquareTile(imagePath: "lib/assets/images/facebook.png"),
-                SizedBox(width: 25,),
-                SquareTile(imagePath: "lib/assets/images/twitter.png"),
+                Expanded(
+                  child: Divider(
+                    thickness: 0.8,
+                    color: Colors.grey[500],
+                  ),
+                ),
+                Text(
+                  "Or continue with",
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                Expanded(
+                  child: Divider(
+                    thickness: 0.8,
+                    color: Colors.grey[500],
+                  ),
+                )
               ],
-            )
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SquareTile(imagePath: "lib/assets/images/google.png"),
+              SizedBox(
+                width: 25,
+              ),
+              SquareTile(imagePath: "lib/assets/images/facebook.png"),
+              SizedBox(
+                width: 25,
+              ),
+              SquareTile(imagePath: "lib/assets/images/twitter.png"),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Not a member? ",
+                style: TextStyle(fontSize: 15),
+              ),
+              GestureDetector(
+                onTap: registerUser,
+                child: Text(
+                  "Register here.",
+                  style: TextStyle(fontSize: 15, color: Colors.blueAccent),
+                ),
+              )
+            ],
+          )
         ],
       ),
     );
